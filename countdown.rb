a80d19dfbb9a1146a67451ac84f3a052246c51af
Don Ward <10is0420@gmail.com>
2018-11-19 23:26:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
 def countdown
+  countdown = 10
+  while countdown < 10
+  puts "#{countdown}SECOND(S)!"
+  countdown -= 1
+end
+
+puts "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep
+  countdown_with_sleep = 5
+  while countdown_with_sleep < 5
+  puts ""
+  countdown_with_sleep -= 1
+end
+
+puts ""
+
 end
